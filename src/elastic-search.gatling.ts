84c4e74d9f261b43ff10367e337aec215dd49cf2
Jakub Szendzielorz <kubasz15@gmail.com>
2025-04-29 11:12:45 +0200

    exclude resources

--- src/elastic-search.gatling.ts
+++ src/elastic-search.gatling.ts
@@ -43,30 +43,30 @@ export default simulation((setUp) => {
   //     .check(jsonPath("$.access_token").saveAs("access_token"))
   // );
 
-  const token = '8815dc88b557672b5da676d9091a73ddb5777c1d';
+  const token = '434639d573a1613f78c3dc5e7611cab529ec8d06';
   // Chain for fetching "Moje sprawy"
-  const getAllIssueWhichContaiWrod_PIT: ChainBuilder = exec(
+  const a: ChainBuilder = exec(
     http("Obłsuwa spraw - PIT")
       .get(
-        "issue/issue_sentence/?fields=Issue_id,Issue_issue_internal_id,Issue_subject,Issue_owner_id,Issue_status,Issue_priority,Issue_tags,Issue_answer_time,Issue_client_info,Issue_modified,Issue_corrected,Issue_issue_initiator_id,Issue_order_id,Issue_offer_send,Issue_archived,Issue_category_id,Issue_sentences_counter,ai_suggest,poll_id&Issue_status=open,new&search=pit&search_in=Issue_id,Issue_subject,IssueSentence_body&search_type=AND&Issue_owner_id=0&order=waiting_time|desc&page=1&limit=10"
+        "issue/issue_sentence/?fields=Issue_id,Issue_issue_internal_id,Issue_subject,Issue_owner_id,Issue_status,Issue_priority,Issue_tags,Issue_answer_time,Issue_client_info,Issue_modified,Issue_corrected,Issue_issue_initiator_id,Issue_order_id,Issue_offer_send,Issue_archived,Issue_category_id,Issue_sentences_counter,ai_suggest,poll_id&Issue_status=open,new&search=a&search_in=Issue_id,Issue_subject,IssueSentence_body&search_type=AND&Issue_owner_id=0&order=waiting_time|desc&page=1&limit=10"
       )
       .header("Authorization", `Bearer ${token}`)
       .check(status().is(200))
   );
 
-  const getAllIssueWhichContaiWrod_PODATEK: ChainBuilder = exec(
+  const b: ChainBuilder = exec(
     http("Obłsuwa spraw - Podatek")
       .get(
-        "issue/issue_sentence/?fields=Issue_id,Issue_issue_internal_id,Issue_subject,Issue_owner_id,Issue_status,Issue_priority,Issue_tags,Issue_answer_time,Issue_client_info,Issue_modified,Issue_corrected,Issue_issue_initiator_id,Issue_order_id,Issue_offer_send,Issue_archived,Issue_category_id,Issue_sentences_counter,ai_suggest,poll_id&Issue_status=open,new&search=podatek&search_in=Issue_id,Issue_subject,IssueSentence_body&search_type=AND&Issue_owner_id=0&order=waiting_time|desc&page=1&limit=10"
+        "issue/issue_sentence/?fields=Issue_id,Issue_issue_internal_id,Issue_subject,Issue_owner_id,Issue_status,Issue_priority,Issue_tags,Issue_answer_time,Issue_client_info,Issue_modified,Issue_corrected,Issue_issue_initiator_id,Issue_order_id,Issue_offer_send,Issue_archived,Issue_category_id,Issue_sentences_counter,ai_suggest,poll_id&Issue_status=open,new&search=b&search_in=Issue_id,Issue_subject,IssueSentence_body&search_type=AND&Issue_owner_id=0&order=waiting_time|desc&page=1&limit=10"
       )
       .header("Authorization", `Bearer ${token}`)
       .check(status().is(200))
   );
 
-  const getAllIssueWhichContaiWrod_VAT: ChainBuilder = exec(
+  const c: ChainBuilder = exec(
     http("Obłsuwa spraw - VAT")
       .get(
-        "issue/issue_sentence/?fields=Issue_id,Issue_issue_internal_id,Issue_subject,Issue_owner_id,Issue_status,Issue_priority,Issue_tags,Issue_answer_time,Issue_client_info,Issue_modified,Issue_corrected,Issue_issue_initiator_id,Issue_order_id,Issue_offer_send,Issue_archived,Issue_category_id,Issue_sentences_counter,ai_suggest,poll_id&Issue_status=open,new&search=vat&search_in=Issue_id,Issue_subject,IssueSentence_body&search_type=AND&Issue_owner_id=0&order=waiting_time|desc&page=1&limit=10"
+        "issue/issue_sentence/?fields=Issue_id,Issue_issue_internal_id,Issue_subject,Issue_owner_id,Issue_status,Issue_priority,Issue_tags,Issue_answer_time,Issue_client_info,Issue_modified,Issue_corrected,Issue_issue_initiator_id,Issue_order_id,Issue_offer_send,Issue_archived,Issue_category_id,Issue_sentences_counter,ai_suggest,poll_id&Issue_status=open,new&search=c&search_in=Issue_id,Issue_subject,IssueSentence_body&search_type=AND&Issue_owner_id=0&order=waiting_time|desc&page=1&limit=10"
       )
       .header("Authorization", `Bearer ${token}`)
       .check(status().is(200))
@@ -76,9 +76,9 @@ export default simulation((setUp) => {
   const scn = scenario("Elastic Search Test")
     // .feed(credentialsFeeder)
     // .exec(getToken) // Execute the token chain
-    .exec(getAllIssueWhichContaiWrod_PIT) // Execute the "Moje sprawy" chain
-    .exec(getAllIssueWhichContaiWrod_PODATEK) // Execute the "Moje sprawy" chain
-    .exec(getAllIssueWhichContaiWrod_VAT); // Execute the "Moje sprawy" chain
+    .exec(a) // Execute the "Moje sprawy" chain
+    .exec(b) // Execute the "Moje sprawy" chain
+    .exec(c); // Execute the "Moje sprawy" chain
 
   setUp(
     scn.injectOpen(
@@ -86,10 +86,10 @@ export default simulation((setUp) => {
       atOnceUsers(10), // 2
       rampUsers(10).during(5), // 3
       constantUsersPerSec(20).during(15), // 4
-      // constantUsersPerSec(20).during(15).randomized(), // 5
-      // rampUsersPerSec(10).to(20).during(10), // 6
-      // rampUsersPerSec(10).to(20).during(10).randomized(), // 7
-      // stressPeakUsers(1000).during(20) // 8
+      constantUsersPerSec(20).during(15).randomized(), // 5
+      rampUsersPerSec(10).to(20).during(10), // 6
+      rampUsersPerSec(10).to(20).during(10).randomized(), // 7
+      stressPeakUsers(1000).during(20) // 8
     )
   ).protocols(httpProtocol);
 });
